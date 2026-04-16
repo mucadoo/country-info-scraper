dfb504e53523ccbed1b27ba6846650f97009e8bb
Samuel Britto <samuelgiordano@live.com>
2026-04-15 22:48:14 -0300

    Removing extraction utility unit tests for area, population, and density from WebScraperTest

--- src/test/java/com/countryinfoscraper/WebScraperTest.java
+++ src/test/java/com/countryinfoscraper/WebScraperTest.java
@@ -18,24 +18,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class WebScraperTest {
     private static final Logger logger = LoggerFactory.getLogger(WebScraperTest.class);
 
-    @Test
-    public void testExtractArea() {
-        assertEquals("123456.78", ExtractionUtils.extractArea("123,456.78 <sup"));
-        assertEquals("123456.78", ExtractionUtils.extractArea("123,456.78&nbsp;km<sup>2</sup>"));
-    }
-
-    @Test
-    public void testExtractPopulation() {
-        assertEquals("1000000", ExtractionUtils.extractPopulation("1,000,000 (2021 estimate)"));
-        assertEquals("1500000", ExtractionUtils.extractPopulation("1-2 million"));
-    }
-
-    @Test
-    public void testExtractDensity() {
-        assertEquals("50", ExtractionUtils.extractDensity("50 <sup class=\"reference\">[1]</sup> / km<sup>2</sup>"));
-        assertEquals("100", ExtractionUtils.extractDensity("100 / km<sup>2</sup>"));
-    }
-
     @Tag("live-watcher")
     @TestFactory
     @DisplayName("Wikipedia Watcher: All Countries")
